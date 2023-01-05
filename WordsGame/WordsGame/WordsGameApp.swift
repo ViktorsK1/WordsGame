8aa8f0b90649e0f55603a4245e5e21372306f037
Viktor Kulia <vitya.kulya@gmail.com>
2023-01-05 15:10:06 +0200

    init project

---
+++ WordsGame/WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Виктор Куля on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
